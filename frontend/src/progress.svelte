880fc9a593c8b594017fb399d8cef92165ce8ba5
Aman Chhabra <aman.chhabra1@gmail.com>
2021-12-07 08:45:40 -0800

    Progress theme update

--- frontend/src/progress.svelte
+++ frontend/src/progress.svelte
@@ -7,16 +7,16 @@
 <div class="progress-outer" transition:slide>
   <div class="progress-inner">
     <div class="mb-1 flex justify-between">
-      <span class="text-base text-blue-700 font-medium dark:text-white"
+      <span class="text-base text-gray-100 font-medium"
         >{status}</span
       >
-      <span class="text-sm font-medium text-blue-700 dark:text-white"
+      <span class="text-sm font-medium text-gray-100"
         >{percent}%</span
       >
     </div>
-    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+    <div class="w-full bg-green-900 rounded-full h-4 shadow-inner">
       <div
-        class="bg-blue-600 h-2.5 rounded-full"
+        class="bg-green-500 h-4 rounded-full animate-pulse"
         style={"width: " + percent.toString() + "%"}
       />
     </div>
