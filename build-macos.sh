5cb4fe45ad8cd6a9c69c32f1721adf1d00bd0fa8
Aman Chhabra <17457975+achhabra2@users.noreply.github.com>
2021-12-16 11:08:21 -0800

    Refactor build scripts

--- build-macos.sh
+++ build-macos.sh
@@ -2,7 +2,7 @@ export PATH=${PATH}:`go env GOPATH`/bin
 echo "building on AMD64"
 wails build -platform darwin/amd64 -clean
 echo "Signing Package"
-gon -log-level=info ./gon-sign.json
+gon -log-level=info ./build/darwin/gon-sign.json
 echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-amd64.zip
 echo "Cleaning up"
@@ -10,10 +10,10 @@ rm -rf ./build/bin/RiftShare.app
 echo "building on ARM64"
 wails build -platform darwin/arm64 -clean
 echo "Signing Package"
-gon -log-level=info -log-json ./gon-sign.json
+gon -log-level=info -log-json ./build/darwin/gon-sign.json
 echo "Zipping Package"
 ditto -c -k --keepParent ./build/bin/RiftShare.app ./RiftShare-darwin-arm64.zip
 echo "Cleaning up"
 rm -rf ./build/bin/RiftShare.app
 echo "Notarizing Zip Files"
-gon -log-level=info ./gon-notarize.json
+gon -log-level=info ./build/darwin/gon-notarize.json
--- internal/update/selfupdate.go
+++ internal/update/selfupdate.go
@@ -7,7 +7,7 @@ import (
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 )
 
-const Version = "0.0.8"
+const Version = "0.1.0"
 
 func DoSelfUpdate() bool {
 	v := semver.MustParse(Version)
