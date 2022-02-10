ac4d441a02d6af4cfa10a9ada10ba7e60039fa42
Aman Chhabra <17457975+achhabra2@users.noreply.github.com>
2022-02-10 11:03:21 -0800

    Remove windows custom dpi code

--- frontend/wailsjs/go/bindings.d.ts
+++ frontend/wailsjs/go/bindings.d.ts
@@ -1,4 +1,4 @@
-interface go {
+export interface go {
   "main": {
     "App": {
 		AppInstalledFromPackageManager():Promise<boolean>
--- internal/settings/dimensions_windows.go
+++ internal/settings/dimensions_windows.go
@@ -1,42 +1,46 @@
 package settings
 
-import (
-	"syscall"
-)
+// import (
+// 	"syscall"
+// )
 
-var (
-	user32           = syscall.NewLazyDLL("User32.dll")
-	getSystemMetrics = user32.NewProc("GetSystemMetrics")
-	getDpiForSystem  = user32.NewProc("GetDpiForSystem")
-)
+// var (
+// 	user32           = syscall.NewLazyDLL("User32.dll")
+// 	getSystemMetrics = user32.NewProc("GetSystemMetrics")
+// 	getDpiForSystem  = user32.NewProc("GetDpiForSystem")
+// )
 
-func GetSystemMetrics(nIndex int) int {
-	index := uintptr(nIndex)
-	ret, _, _ := getSystemMetrics.Call(index)
-	return int(ret)
-}
+// func GetSystemMetrics(nIndex int) int {
+// 	index := uintptr(nIndex)
+// 	ret, _, _ := getSystemMetrics.Call(index)
+// 	return int(ret)
+// }
 
-func GetDpiForSystem() int {
-	ret, _, _ := getDpiForSystem.Call()
-	return int(ret)
-}
+// func GetDpiForSystem() int {
+// 	ret, _, _ := getDpiForSystem.Call()
+// 	return int(ret)
+// }
 
-const (
-	SM_CXSCREEN = 0
-	SM_CYSCREEN = 1
-)
+// const (
+// 	SM_CXSCREEN = 0
+// 	SM_CYSCREEN = 1
+// )
 
-func GetResolution() (int, int) {
-	return GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)
-}
+// func GetResolution() (int, int) {
+// 	return GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)
+// }
+
+// func GetAppDefaultDimensionsBackup() (int, int) {
+// 	w := 528.0
+// 	h := 440.0
+// 	baseDpi := 96.0
+// 	currentDpi := float64(GetDpiForSystem())
+// 	scalingFactor := currentDpi / baseDpi
+// 	w2 := int(w * scalingFactor)
+// 	h2 := int(h * scalingFactor)
+// 	return w2, h2
+// }
 
 func GetAppDefaultDimensions() (int, int) {
-	w := 528.0
-	h := 440.0
-	baseDpi := 96.0
-	currentDpi := float64(GetDpiForSystem())
-	scalingFactor := currentDpi / baseDpi
-	w2 := int(w * scalingFactor)
-	h2 := int(h * scalingFactor)
-	return w2, h2
+	return 528, 440
 }
