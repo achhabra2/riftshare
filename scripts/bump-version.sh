ddc10905d864ce07221cb371beb5e134d16834e1
Aman Chhabra <17457975+achhabra2@users.noreply.github.com>
2022-06-06 10:38:01 -0700

    Move scripts into a scripts folder

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo $PWD
 sed -i '' s/$1/$2/g ./build/darwin/Info.plist
 sed -i '' s/$1/$2/g ./internal/update/selfupdate.go
 sed -i '' s/$1\.0/$2\.0/g ./build/windows/appxmanifest.xml
