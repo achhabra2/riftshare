d2597c53274e76039df5c537a74eae9a3f1cf21c
Aman Chhabra <17457975+achhabra2@users.noreply.github.com>
2022-06-06 10:42:30 -0700

    Add info to bump-version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo $PWD
+echo "Bumping version from ${1} to ${2}"
+echo "Replacing version in Info.plist"
 sed -i '' s/$1/$2/g ./build/darwin/Info.plist
+echo "Replacing version in selfupdate.go"
 sed -i '' s/$1/$2/g ./internal/update/selfupdate.go
+echo "Replacing version in appxmanifest.xml"
 sed -i '' s/$1\.0/$2\.0/g ./build/windows/appxmanifest.xml
+echo "Replacing version in RiftShare.exe.manifest"
 sed -i '' s/$1\.0/$2\.0/g ./build/windows/RiftShare.exe.manifest
