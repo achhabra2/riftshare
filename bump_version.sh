b998bf4a26d932f59f495ce8eba9db18a08250b9
Aman Chhabra <17457975+achhabra2@users.noreply.github.com>
2022-06-06 10:28:19 -0700

    Remove unnecessary sed statement

--- bump_version.sh
+++ bump_version.sh
@@ -4,4 +4,3 @@ sed -i '' s/$1/$2/g ./build/darwin/Info.plist
 sed -i '' s/$1/$2/g ./internal/update/selfupdate.go
 sed -i '' s/$1\.0/$2\.0/g ./build/windows/appxmanifest.xml
 sed -i '' s/$1\.0/$2\.0/g ./build/windows/RiftShare.exe.manifest
-sed -i '' s/$1/$2/g ./build/linux/app.riftshare.RiftShare.desktop
